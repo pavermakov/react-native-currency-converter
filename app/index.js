c5223acc10e7e87e893fadc7ee1f0946b55a444b
Pavel Ermakov <Pavel1994-09@mail.ru>
2018-03-30 12:37:04 +0300

    MOBILE-4. Added text fields

--- app/index.js
+++ app/index.js
@@ -4,7 +4,10 @@ import Home from './screens/Home';
 
 EStyleSheet.build({
   $primaryBlue: '#4F6D7A',
-  $white: '#fff',
+  $white: '#FFF',
+  $lightGray: '#F0F0F0',
+  $border: '#E2E2E2',
+  $inputText: '#797979',
 });
 
 export default () => <Home />;
