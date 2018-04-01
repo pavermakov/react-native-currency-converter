e116c7c30c1dca1cad997be63a99ec3fe694ba9d
pavermakov <pavermakov@gmail.com>
2018-04-01 23:55:00 +0300

    MOBILE-7. Added header icon

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -5,6 +5,7 @@ import { Logo } from '../components/Logo';
 import { InputWithButton } from '../components/TextInput';
 import { ClearButton } from '../components/Buttons';
 import { LastConverted } from '../components/Text';
+import { Header } from '../components/Header';
 
 const TEMP_BASE_CURRENCY = 'USD';
 const TEMP_QUOTE_CURRENCY = 'GPB';
@@ -30,6 +31,10 @@ class Home extends Component {
     console.log('handleSwapCurrency');
   };
 
+  handleOptionsPress = () => {
+    console.log('handleOptionsPress');
+  };
+
   render() {
     return (
       <Container>
@@ -37,6 +42,8 @@ class Home extends Component {
           translucent={false}
           barStyle='light-content'
         />
+
+        <Header onPress={this.handleOptionsPress} />
 
         <Logo />
 
