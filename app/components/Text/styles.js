93e72fc97828e2737c572e8999465fe71b2de65f
Pavel Ermakov <Pavel1994-09@mail.ru>
2018-04-01 23:26:43 +0300

    MOBILE-6. Added exchange rate text

---
+++ app/components/Text/styles.js
@@ -0,0 +1,9 @@
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+export default EStyleSheet.create({
+  smallText: {
+    color: '$white',
+    fontSize: 12,
+    textAlign: 'center',
+  },
+});
--- app/screens/Home.js
+++ app/screens/Home.js
@@ -4,11 +4,14 @@ import { Container } from '../components/Container';
 import { Logo } from '../components/Logo';
 import { InputWithButton } from '../components/TextInput';
 import { ClearButton } from '../components/Buttons';
+import { LastConverted } from '../components/Text';
 
 const TEMP_BASE_CURRENCY = 'USD';
 const TEMP_QUOTE_CURRENCY = 'GPB';
 const TEMP_BASE_PRICE = '100';
 const TEMP_QUOTE_PRICE = '79.74';
+const TEMP_CONVERSION_RATE = 0.7974;
+const TEMP_CONVERSION_DATE = new Date();
 
 class Home extends Component {
   handlePressBaseCurrency = () => {
@@ -50,6 +53,13 @@ class Home extends Component {
           onPress={this.handlePressQuoteCurrency}
           editable={false}
           value={TEMP_QUOTE_PRICE}
+        />
+
+        <LastConverted
+          date={TEMP_CONVERSION_DATE}
+          base={TEMP_BASE_CURRENCY}
+          quote={TEMP_QUOTE_CURRENCY}
+          conversionRate={TEMP_CONVERSION_RATE}
         />
 
         <ClearButton
