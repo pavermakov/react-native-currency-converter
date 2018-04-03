e0668f59a3da616186c565f173642e09814cf985
pavermakov <pavermakov@gmail.com>
2018-04-03 16:38:56 +0300

    MOBILE-10. Added options screen

---
+++ app/screens/Options.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { ScrollView, StatusBar, Platform } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { ListItem, Separator } from '../components/List';
+
+const ICON_PREFIX = Platform.OS === 'ios' ? 'ios' : 'md';
+const ICON_COLOR = '#868686';
+const ICON_SIZE = 23;
+
+const themeIcon = (
+  <Ionicons
+    name={`${ICON_PREFIX}-arrow-forward`}
+    color={ICON_COLOR}
+    size={ICON_SIZE}
+  />
+);
+
+const linkIcon = (
+  <Ionicons
+    name={`${ICON_PREFIX}-link`}
+    color={ICON_COLOR}
+    size={ICON_SIZE}
+  />
+);
+
+class Options extends Component {
+  handleThemesPress = () => {
+    console.log('press themes');
+  };
+
+  handleSitePress = () => {
+    console.log('press site');
+  };
+
+  render() {
+    return (
+      <ScrollView>
+        <StatusBar
+          barStyle="default"
+          translucent={false}
+        />
+
+        <ListItem
+          text="Themes"
+          customIcon={themeIcon}
+          onPress={this.handleThemesPress}
+        />
+
+        <Separator />
+
+        <ListItem
+          text="Fixer.io"
+          customIcon={linkIcon}
+          onPress={this.handleSitePress}
+        />
+
+        <Separator />
+      </ScrollView>
+    );
+  }
+}
+
+export default Options;
