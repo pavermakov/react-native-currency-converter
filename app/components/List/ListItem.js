01de5199101f94333f970eb24a02d5609fae37e7
Pavel Ermakov <Pavel1994-09@mail.ru>
2018-04-03 16:40:18 +0300

    MOBILE-10. Added options screen

--- app/components/List/ListItem.js
+++ app/components/List/ListItem.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import Icon from './Icon';
 import styles from './styles';
 
-const ListItem = ({ text, selected = false, onPress, checkmark = true, visible = true }) => (
+const ListItem = ({ text, selected = false, onPress, checkmark = true, visible = true, customIcon = null }) => (
   <TouchableHighlight
     underlayColor={styles.$underlayColor}
     onPress={onPress}
@@ -12,6 +12,7 @@ const ListItem = ({ text, selected = false, onPress, checkmark = true, visible =
     <View style={styles.row}>
       <Text style={styles.text}>{text}</Text>
       {selected ? <Icon checkmark={checkmark} visible={visible} /> : <Icon />}
+      {customIcon}
     </View>
   </TouchableHighlight>
 );
@@ -22,6 +23,7 @@ ListItem.propTypes = {
   checkmark: PropTypes.bool,
   visible: PropTypes.bool,
   onPress: PropTypes.func,
+  customIcon: PropTypes.element,
 };
 
 export default ListItem;
