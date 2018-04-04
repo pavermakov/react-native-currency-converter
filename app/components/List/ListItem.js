0baa6965fabfe343723180a4adc665ce55b25c99
pavermakov <pavermakov@gmail.com>
2018-04-04 16:37:38 +0300

    MOBILE-11. Added themes screens

--- app/components/List/ListItem.js
+++ app/components/List/ListItem.js
@@ -4,26 +4,39 @@ import PropTypes from 'prop-types';
 import Icon from './Icon';
 import styles from './styles';
 
-const ListItem = ({ text, selected = false, onPress, checkmark = true, visible = true, customIcon = null }) => (
-  <TouchableHighlight
-    underlayColor={styles.$underlayColor}
-    onPress={onPress}
-  >
-    <View style={styles.row}>
-      <Text style={styles.text}>{text}</Text>
-      {selected ? <Icon checkmark={checkmark} visible={visible} /> : <Icon />}
-      {customIcon}
-    </View>
-  </TouchableHighlight>
-);
+const ListItem = (props) => {
+  const {
+    checkmark = true,
+    customIcon = null,
+    selected = false,
+    visible = true,
+    iconBackground,
+    text,
+    onPress,
+  } = props;
+
+  return (
+    <TouchableHighlight
+      underlayColor={styles.$underlayColor}
+      onPress={onPress}
+    >
+      <View style={styles.row}>
+        <Text style={styles.text}>{text}</Text>
+        {selected ? <Icon checkmark={checkmark} visible={visible} iconBackground={iconBackground} /> : <Icon />}
+        {customIcon}
+      </View>
+    </TouchableHighlight>
+  );
+};
 
 ListItem.propTypes = {
   text: PropTypes.string,
   selected: PropTypes.bool,
   checkmark: PropTypes.bool,
   visible: PropTypes.bool,
-  onPress: PropTypes.func,
+  iconBackground: PropTypes.string,
   customIcon: PropTypes.element,
+  onPress: PropTypes.func,
 };
 
 export default ListItem;
