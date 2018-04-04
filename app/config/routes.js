b30a5003af77e21011cadb3cd0f616865cd0cfa7
Pavel Ermakov <Pavel1994-09@mail.ru>
2018-04-04 20:17:33 +0300

    MOBILE-13. Added navigation to options and themes

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,20 +1,49 @@
 import { StatusBar } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import Home from '../screens/Home';
+import Options from '../screens/Options';
+import Themes from '../screens/Themes';
 import CurrencyList from '../screens/CurrencyList';
 
-export default StackNavigator({
+const HomeStack = StackNavigator({
   Home: {
     screen: Home,
     navigationOptions: {
       header: () => null,
     },
   },
+  Options: {
+    screen: Options,
+    navigationOptions: {
+      headerTitle: 'Options',
+    },
+  },
+  Themes: {
+    screen: Themes,
+    navigationOptions: {
+      headerTitle: 'Themes',
+    },
+  },
+}, {
+  headerMode: 'screen',
+});
+
+const CurrencyListStack = StackNavigator({
   CurrencyList: {
     screen: CurrencyList,
     navigationOptions: ({ navigation }) => ({ headerTitle: navigation.state.params.title }),
+  }
+});
+
+export default StackNavigator({
+  Home: {
+    screen: HomeStack,
+  },
+  CurrencyList: {
+    screen: CurrencyListStack,
   },
 }, {
   mode: 'modal',
+  headerMode: 'none',
   cardStyle: { paddingTop: StatusBar.currentHeight },
 });
