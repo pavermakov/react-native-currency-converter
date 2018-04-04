87d75a1834a80b5e869c3d3256d75ab9325c03f9
Pavel Ermakov <Pavel1994-09@mail.ru>
2018-04-04 22:01:24 +0300

    MOBILE-14. Added alert messages

--- app/screens/Options.js
+++ app/screens/Options.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { ScrollView, StatusBar, Platform } from 'react-native';
+import { ScrollView, StatusBar, Platform, Linking } from 'react-native';
 import PropTypes from 'prop-types';
 import { Ionicons } from '@expo/vector-icons';
 import { ListItem, Separator } from '../components/List';
+import { connectAlert } from '../components/Alert';
 
 const ICON_PREFIX = Platform.OS === 'ios' ? 'ios' : 'md';
 const ICON_COLOR = '#868686';
@@ -27,6 +28,7 @@ const linkIcon = (
 class Options extends Component {
   static propTypes = {
     navigation: PropTypes.object,
+    alertWithType: PropTypes.func,
   };
 
   handleThemesPress = () => {
@@ -34,7 +36,11 @@ class Options extends Component {
   };
 
   handleSitePress = () => {
-    console.log('press site');
+    Linking
+      .openURL('http://fixer.io')
+      .catch(() => {
+        this.props.alertWithType('error', 'Sorry!', 'The requested link cannot be opened.')
+      });
   };
 
   render() {
@@ -65,4 +71,4 @@ class Options extends Component {
   }
 }
 
-export default Options;
+export default connectAlert(Options);
