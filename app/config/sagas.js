d278245d75c7429bc09434cd0d0aec9497e642e1
pavermakov <pavermakov@gmail.com>
2018-04-08 23:17:39 +0300

    MOBILE-17. Added redux-saga

---
+++ app/config/sagas.js
@@ -0,0 +1,38 @@
+import { takeEvery, select, call, put } from 'redux-saga/effects';
+
+import {
+  SWAP_CURRENCY,
+  CHANGE_BASE_CURRENCY,
+  GET_INITIAL_CONVERSION,
+  CONVERSION_RESULT,
+  CONVERSION_ERROR,
+} from '../actions/currencies';
+
+const getLatestRates = currency => fetch(`http://api.fixer.io/latest?base=${currency}`);
+
+function* fetchLatestConversionRates(action) {
+  try {
+    let { currency } = action;
+
+    if (currency === undefined) {
+      currency = yield select(state => state.currencies.baseCurrency);
+    }
+
+    const response = yield call(getLatestRates, currency);
+    const result = yield response.json();
+
+    if (result.error) {
+      yield put({ type: CONVERSION_ERROR, error: result.error });
+    } else {
+      yield put({ type: CONVERSION_RESULT, result });
+    }
+  } catch (error) {
+    yield put({ type: CONVERSION_ERROR, error: error.message });
+  }
+}
+
+export default function* rootSaga() {
+  yield takeEvery(GET_INITIAL_CONVERSION, fetchLatestConversionRates);
+  yield takeEvery(SWAP_CURRENCY, fetchLatestConversionRates);
+  yield takeEvery(CHANGE_BASE_CURRENCY, fetchLatestConversionRates);
+}
--- app/screens/Home.js
+++ app/screens/Home.js
@@ -10,7 +10,7 @@ import { ClearButton } from '../components/Buttons';
 import { LastConverted } from '../components/Text';
 import { Header } from '../components/Header';
 
-import { swapCurrency, changeCurrencyAmount } from '../actions/currencies';
+import { swapCurrency, changeCurrencyAmount, getInitialConversion } from '../actions/currencies';
 
 class Home extends Component {
   static propTypes = {
@@ -24,6 +24,10 @@ class Home extends Component {
     lastConvertedDate: PropTypes.object,
     primaryColor: PropTypes.string,
   };
+
+  componentWillMount() {
+    this.props.dispatch(getInitialConversion());
+  }
 
   handlePressBaseCurrency = () => {
     this.props.navigation.navigate('CurrencyList', { title: 'Base Currency', type: 'base' });
