346b19901ce953cf45dd76a40a73bacf1b0c2ff9
Pavel Ermakov <Pavel1994-09@mail.ru>
2018-04-08 23:18:39 +0300

    MOBILE-17. Added redux-saga

--- app/config/store.js
+++ app/config/store.js
@@ -1,11 +1,21 @@
 import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import logger from 'redux-logger';
+
 import reducers from '../reducers';
+import rootSaga from './sagas';
 
 const middleware = [];
+const sagaMiddleware = createSagaMiddleware();
+
+middleware.push(sagaMiddleware);
 
 if (process.env.NODE_ENV === 'development') {
   middleware.push(logger);
 }
 
-export default createStore(reducers, applyMiddleware(...middleware));
+const store = createStore(reducers, applyMiddleware(...middleware));
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
